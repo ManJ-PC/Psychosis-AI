ccf61bdffece7781b2f4347b1ff282077e0fd42d
Camolas <up201202445@fe.up.pt>
2020-02-05 19:53:12 +0000

    Update Webcam Program

--- Webcam.py
+++ Webcam.py
@@ -1,14 +1,37 @@
 import cv2, time
  
 #1. Create an object. Zero for external camera
-video=cv2.VideoCapture(0)
+video = cv2.VideoCapture(0)
+
+#8. a variable
+a=0
  
-#3. Create a frame object
-check, frame = video.read()
- 
-print(check)
-print(frame) #Representing image
- 
- 
+while True:
+    a = a+1
+    #3. Create a frame object
+    check, frame = video.read()
+
+    print(check)
+    print(frame)  # Representing image
+
+    #6. Converting to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    #4 Show the frame!
+
+    cv2.imshow("Capturing", gray)
+
+    #5.For press any key to out (miliseconds)
+    #cv2.waitKey(0)
+
+    #7. For playing
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+print(a)
 #2. Shutdown the camera
 video.release()
+
+cv2.destroyAllWindows
